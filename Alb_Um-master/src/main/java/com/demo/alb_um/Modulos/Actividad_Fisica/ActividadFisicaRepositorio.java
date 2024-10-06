1e439d9d90f89782844905aef71bf3bde582074b
Hithan-01 <82106479+Hithan-01@users.noreply.github.com>
2024-10-06 13:48:02 -0600

    Delete Alb_Um-master directory

--- Alb_Um-master/src/main/java/com/demo/alb_um/Modulos/Actividad_Fisica/ActividadFisicaRepositorio.java
+++
@@ -1,8 +0,0 @@
-package com.demo.alb_um.Modulos.Actividad_Fisica;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ActividadFisicaRepositorio extends JpaRepository<Entidad_ActividadFisica, Long> {
-}
--- Alb_Um-master/src/main/java/com/demo/alb_um/Modulos/Listas/Repositorio_lista.java
+++
@@ -1,7 +0,0 @@
-package com.demo.alb_um.Modulos.Listas;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface Repositorio_lista extends JpaRepository<Entidad_Lista, Long> {
-}
